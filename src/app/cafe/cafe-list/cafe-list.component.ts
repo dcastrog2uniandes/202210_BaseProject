adb4c55469feae0445fe4d1c3d2649a5da16e1fb
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2022-04-29 17:42:29 -0500

    Punto 2: se incluye totales para tipo de cafe: blend y de origen

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,12 +10,17 @@ import { CafeService } from '../cafe.service';
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  cafeBlend: number = 0;
+  cafeOrigen: number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes():void{
     this.cafeService.getCafes().subscribe( (cafes) =>{
       this.cafes = cafes;
+      this.cafeBlend = this.cafes.filter( p => p.tipo === 'Blend').length;
+      this.cafeOrigen = this.cafes.filter( p => p.tipo === 'Café de Origen').length;
+
     });
   }
 
